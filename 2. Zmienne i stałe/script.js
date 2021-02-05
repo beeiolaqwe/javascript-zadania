73bb1abe3e20ed2965750340788f7ae4973adc72
beeiolaqwe <32040908+beeiolaqwe@users.noreply.github.com>
2021-02-05 19:30:07 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,21 @@
+// zadanie 1
+var liczba = 3;
+var str = "Hello world";
+var number_str = 2 + "logitech";
+var logic = true;
+var special = {test: 'no'};
+
+console.log(liczba);
+console.log(str);
+console.log(number_str);
+console.log(logic);
+console.log(special);
+
+// zadanie 2
+var a = 11, b = 5, sum = 0;
+console.log(sum = 11 + 5);
+
+// zadanie 3
+var test;
+console.log(test);
+// undefined, bo zmienna nie została zdefiniowana
